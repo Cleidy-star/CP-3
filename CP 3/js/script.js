c46ab1ec24ce49b0cc166f9b2acf9daed02dfbb9
Cleidy-Star <polaris.zm16@gmail.com>
2024-05-26 22:33:13 -0300

    garantindo que o usuario não deixe o espaço em branco e criando uma nova tarefa

--- CP 3/js/script.js
+++ CP 3/js/script.js
@@ -4,4 +4,12 @@ e.preventDefault();
 const taskInput = document.getElementById('new-task');
 const taskTexte = taskInput.value.trim();
 
+if(taskTexte !==''){
+    const taskList = document.getElementById('todo-list');
+    const taskItem = document.createElement('li');
+    taskItem.className = 'task-item';
+
+    const
+}
+
 }
