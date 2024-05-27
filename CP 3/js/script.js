d270dfe58d6a127a1f4a569dad8bc9fb5ed241be
Cleidy-Star <polaris.zm16@gmail.com>
2024-05-26 22:36:30 -0300

    recebendo o texto do usuario

--- CP 3/js/script.js
+++ CP 3/js/script.js
@@ -4,12 +4,6 @@ e.preventDefault();
 const taskInput = document.getElementById('new-task');
 const taskTexte = taskInput.value.trim();
 
-if(taskTexte !==''){
-    const taskList = document.getElementById('todo-list');
-    const taskItem = document.createElement('li');
-    taskItem.className = 'task-item';
 
-    const
-}
 
 }
