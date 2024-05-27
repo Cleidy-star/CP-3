7d6978e1a7399b135a93bc50846d042b258a12f4
Cleidy-Star <polaris.zm16@gmail.com>
2024-05-26 22:29:15 -0300

    recebendo o texto do usuario

---
+++ CP 3/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('todo-form').addEventListener('submit', function(e)){
+e.preventDefault();
+
+const taskInput = document.getElementById('new-task');
+const taskTexte = taskInput.value.trim();
+
+}
